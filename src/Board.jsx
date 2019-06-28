c46cfe14c3c9e16542ab1dbfbfc1ec4a2ec9444d
Nour <nour.benhassen@gmail.com>
2019-06-28 19:09:46 +0200

    functions added in different file called functions.js to make code cleaner

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,12 @@
 import React from "react"
 import Square from "./Square"
+import functions from "./functions"
+//var functions = require('./functions.js');
+// functions import not working - problem coming from this keyword ?
 
+
+
+// to correct: when X plays O doesn't see immediately new Board, but needs to wait a round after to be able to see it ==> state is not updated before computer plays
 
 class Board extends React.Component {
     constructor (props) {
@@ -14,7 +20,7 @@ class Board extends React.Component {
       const squares = this.state.squares.slice()
       if(!squares[i]){ 
         squares[i]='X';
-        let j = AISquare(this.state.squares);
+        let j = functions.AISquare(this.state.squares);  //MODIFICATION HERE
         squares[j]='O';
       }
       
@@ -32,7 +38,7 @@ class Board extends React.Component {
   
     render() {
       
-      const winner = calculateWinner(this.state.squares);
+      const winner = functions.calculateWinner(this.state.squares);   //MODIFICATION HERE
       let status;
       if (winner) {
           status = 'Winner: ' + winner;
@@ -62,71 +68,5 @@ class Board extends React.Component {
       );
     }
   }
-
-
-//function to check if there's a winner
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  //function takes random Empty square
-  function randomEmptySquare(squares) {
-    let j = Math.floor(Math.random() * 9);     //chooses random number between 0 and 8
-    if (squares[j]===null) return j;        //if square empty returns j
-    else return randomEmptySquare(squares);      //if square not empty call function again until empty j is found - recursion
-
-}
-
-
-  //function to find best strategic square when computer plays
-  function AISquare(squares) {
-    //chooses best strategic number between 0 and 8
-    // Strategy:if 2 "X" are part of a winning combination take the 3rd square
-    // if not take random number instead
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
-
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-
-            if (squares[a]){
-                if (squares[a]===squares[b] && !squares[c]) return c;
-                else if (squares[a]===squares[c] && !squares[b]) return b;
-        }
-            else if(squares[b]) {
-                if (squares[b]===squares[c] && !squares[a]) return a;
-                else if (squares[b]===squares[a] && !squares[c]) return c;
-            }
-      }
-      return randomEmptySquare(squares);
-    }
-}
-
-
-/* to correct: when X plays O doesn't see immediately new Board, but needs to wait a round after to be able to see it ==> state is not updated before computer plays
 
   export default Board
